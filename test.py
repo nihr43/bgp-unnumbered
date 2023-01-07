564da3e6af96289e98af383e6d3e8500bcafd443
nathan hensel <nathanihensel@gmail.com>
2023-01-07 16:59:44 -0600

    torward virtualized regression testing

---
+++ test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+import uuid
+import time
+
+
+def cleanup(client, log):
+    instances_to_delete = []
+    for i in client.instances.all():
+        if i.name.startswith('bgp-unnumbered-'):
+            log.info('found ' + i.name)
+            instances_to_delete.append(i)
+
+    for i in instances_to_delete:
+        i.stop(wait=True)
+        i.delete()
+        log.info(i.name + ' deleted')
+
+
+def create_node(client, name, log):
+    name = 'bgp-unnumbered-' + name + '-' + str(uuid.uuid4())[0:5]
+    config = {'name': name,
+              'source': {'type': 'image',
+                         'mode': 'pull',
+                         'server': 'https://images.linuxcontainers.org',
+                         'protocol': 'simplestreams',
+                         'alias': 'debian/12'},
+              'config': {'limits.cpu': '2',
+                         'limits.memory': '1GB'},
+              'type': 'virtual-machine'}
+    log.info('creating node ' + name)
+    inst = client.instances.create(config, wait=True)
+    inst.start(wait=True)
+    wait_until_ready(inst, log)
+    return inst
+
+
+def wait_until_ready(instance, log):
+    '''
+    waits until an instance is executable
+    '''
+    count = 30
+    for i in range(count):
+        if instance.execute(['hostname']).exit_code == 0:
+            break
+        if i == count-1:
+            log.info('timed out waiting')
+            exit(1)
+        log.info('waiting for lxd agent on ' + instance.name)
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    def privileged_main():
+        import pylxd
+        import logging
+        import argparse
+
+        logging.basicConfig(level=logging.INFO,
+                            format='%(funcName)s(): %(message)s')
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.INFO)
+        client = pylxd.Client()
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--create', action='store_true')
+        parser.add_argument('--cleanup', action='store_true')
+        parser.add_argument('--spines', type=int, default=2)
+        parser.add_argument('--leafs', type=int, default=3)
+        args = parser.parse_args()
+
+        if args.create:
+            for i in range(args.spines):
+                create_node(client, 'spine', log)
+            for i in range(args.leafs):
+                create_node(client, 'leaf', log)
+        elif args.cleanup:
+            cleanup(client, log)
+
+    privileged_main()

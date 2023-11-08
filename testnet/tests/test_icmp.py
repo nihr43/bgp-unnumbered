7e798c590e86600c87faa8f217e9464493bd7224
nathan hensel <nathanihensel@gmail.com>
2023-11-07 21:11:39 -0600

    dont bother with spines

--- testnet/tests/test_icmp.py
+++ testnet/tests/test_icmp.py
@@ -9,23 +9,13 @@ def test_icmp():
     log = logging.getLogger(__name__)
     client = pylxd.Client()
 
-    spines = []
     leafs = []
     for n in testnet.get_nodes(client, log):
         js = json.loads(n.description)
-        if js["role"] == "spine":
-            spines.append(n)
-        elif js["role"] == "leaf":
+        if js["role"] == "leaf":
             leafs.append(n)
 
-    for i in spines + leafs:
-        log.info(
-            "found router {} ip {}".format(
-                i.name, i.state().network["lo"]["addresses"][1]["address"]
-            )
-        )
-
-    # each router should be able to reach every other router via icmp
+    # each leaf should be able to reach every other leaf via icmp
     for i in leafs:
         for j in leafs:
             if j != i:
